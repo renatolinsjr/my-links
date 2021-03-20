f4127752435efa3f580d45644a2fba603f68d0b6
renatolins <renatolinsren@gmail.com>
2021-03-20 15:54:58 -0300

    change: description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,7 +16,7 @@ function App({ Component, pageProps }: AppProps) {
         <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="Coleção dos links sociais de Renato Lins Giraldes Junior"
         />
       </Head>
       <GlobalStyles />
