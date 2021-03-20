171757928505ef2e67840d716cacd621c23de1f8
renatolins <renatolinsren@gmail.com>
2021-03-19 23:55:18 -0300

    💄add: theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,43 @@
+export default {
+  grid: {
+    container: '76.8rem'
+  },
+  border: {
+    radius: '0.4rem'
+  },
+  font: {
+    family:
+      "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    bold: 600,
+    sizes: {
+      xxsmall: '1.2rem',
+      xsmall: '1.4rem',
+      small: '1.6rem',
+      medium: '1.8rem',
+      large: '2rem',
+      xlarge: '2.2rem',
+      xxlarge: '2.4rem'
+    }
+  },
+  colors: {
+    primary: '#348a7f',
+    primaryDark: '#1b5a5a',
+    highlight: '#01a79b',
+    highlightDark: '#007569',
+    mainBg: '#030518',
+    boxBg: '#FAFAFA',
+    texts: '#030517',
+    white: '#FFFFFF',
+    black: '#000000',
+    gray: '#7B7373'
+  },
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '6.0rem',
+    xlarge: '6.4rem',
+    xxlarge: '12.8rem'
+  }
+}
