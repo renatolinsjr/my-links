9e5d3063746a9f05cb493f468cc84616c2fdb1f6
renatolins <renatolinsren@gmail.com>
2021-03-21 19:17:15 -0300

    ✨change: Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,22 @@
 import LinkBox from 'components/LinkBox'
+import Container from 'components/Container'
 
 export default function Home() {
-  return <LinkBox>kkk</LinkBox>
+  return (
+    <Container>
+      <LinkBox highlight>Site (desatualizado)</LinkBox>
+      <LinkBox icon="github" href="https://github.com/renatolinsjr">
+        Github
+      </LinkBox>
+      <LinkBox icon="linkedin" href="https://www.linkedin.com/in/renatolinsjr/">
+        LinkedIn
+      </LinkBox>
+      <LinkBox
+        icon="facebook"
+        href="https://www.facebook.com/profile.php?id=100008647985839"
+      >
+        Facebook
+      </LinkBox>
+    </Container>
+  )
 }
